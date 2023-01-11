901a14ef453e6213ee91b8cd32d58b5b66da179c
Sashka Teova <sashka_aleksandrova@abv.bg>
2023-01-12 00:22:37 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.StacksAndQueues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+
+            var stack= new Stack<char>();
+
+            foreach (var character in input)
+            {
+                stack.Push(character);  
+            }
+
+            while(stack.Count > 0)
+            {
+                Console.Write(stack.Pop());
+            }
+            Console.WriteLine();
+          
+
+
+        }
+    }
+}
