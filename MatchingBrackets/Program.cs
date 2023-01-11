ed9fb2be9c23f512d22c8c68a88a67b30060b199
sashka <sashka_aleksandrova@abv.bg>
2023-01-11 14:36:22 +0200

    new project

---
+++ MatchingBrackets/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatchingBrackets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input=Console.ReadLine();
+            var stack= new Stack<int>();
+
+            for (int i = 0; i < input.Length-1; i++)
+            {
+                if (input[i]=='(')
+                {
+                    stack.Push(i);  
+                   
+                }
+                if (input[i]==')')
+                {
+                  int openBracket= stack.Pop();
+
+                    for (int j = openBracket+1; j < i; j++)
+                    {
+                        Console.Write(input[j]);
+                    }
+                    Console.WriteLine();
+                }
+                {
+
+                }
+            }
+
+        }
+    }
+}
---
+++ StackSum/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            var stack = new Stack<int>();
+            foreach (var item in input)
+            {
+                stack.Push(item);
+            }
+
+            var command = Console.ReadLine().ToLower();
+            
+            while (command !="end")
+            {
+                var splitedCommand = command.Split(' ').ToList();
+              
+                if (splitedCommand[0]=="add")
+                {
+                    var first = int.Parse(splitedCommand[1]);
+                    var second = int.Parse(splitedCommand[2]);
+
+                    stack.Push(first);
+                    stack.Push(second);
+                }
+
+                if (splitedCommand[0] == "remove")
+                {
+                    var n=int.Parse(splitedCommand[1]);
+
+                    if (n <= stack.Count)
+                    {
+                        for (int i = 0; i < n; i++)
+                        {
+                            stack.Pop();
+                        }
+                    }
+                }
+                command = Console.ReadLine().ToLower();
+            }
+            Console.WriteLine("Sum: {0}",stack.Sum());
+
+        }
+    }
+}
